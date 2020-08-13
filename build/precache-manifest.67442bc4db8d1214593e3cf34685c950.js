2528ad959339a71068947087dc7ac9256ad0f6a3
robertasliekis <60491171+robertasliekis@users.noreply.github.com>
2020-08-13 15:37:39 +0300

    Add files via upload

---
+++ build/precache-manifest.67442bc4db8d1214593e3cf34685c950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c504d024809bfd6481a5a8edc1ed990",
+    "url": "/hero-search/index.html"
+  },
+  {
+    "revision": "ff29277de06c8e8d4641",
+    "url": "/hero-search/static/css/main.b97fd215.chunk.css"
+  },
+  {
+    "revision": "5b99631916d7caffb6d7",
+    "url": "/hero-search/static/js/2.42b06f48.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/hero-search/static/js/2.42b06f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff29277de06c8e8d4641",
+    "url": "/hero-search/static/js/main.58b32217.chunk.js"
+  },
+  {
+    "revision": "09c15eb158f5cf0aeb31",
+    "url": "/hero-search/static/js/runtime-main.cc00b252.js"
+  },
+  {
+    "revision": "a1409b3f30706b751bdacb252efc5f3d",
+    "url": "/hero-search/static/media/marvel-logo.a1409b3f.png"
+  }
+]);
